c2d1c36fd2d9907af173e730680821669a4ab139
ndominik12 <ndomi560@gmail.com>
2026-03-04 11:09:54 +0100

    feladat 3 is ready

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace szamlalas
     {
         static int[] meres = { 36, 48, 39, -1, 30, 43, -1, 76, 67, 82, 73, 75, 64, 73, 69, 63 };
 
+        static int Ora(int i) { return 6 + i / 4; }
+
         static void Main(string[] args)
         {
             int osszeg = 0;
@@ -18,6 +20,17 @@ namespace szamlalas
 
             Console.WriteLine("2. feladat");
             Console.WriteLine("Osszesen " + osszeg + " kerekparost szamoltak.");
+
+            Console.WriteLine();
+            Console.WriteLine("3. feladat");
+            Console.WriteLine("Orankenti meresek:");
+            for (int h = 6; h <= 9; h++)
+            {
+                int oraiOsszeg = 0;
+                for (int i = 0; i < meres.Length; i++)
+                    if (Ora(i) == h && meres[i] != -1) oraiOsszeg += meres[i];
+                Console.WriteLine(h + " oratol " + oraiOsszeg + " kerekparos");
+            }
         }
     }
 }
