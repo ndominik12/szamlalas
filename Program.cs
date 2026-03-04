50eda460fd0f4a13edabf3c864e967c5a7c124dc
ndominik12 <ndomi560@gmail.com>
2026-03-04 11:07:29 +0100

    feladat 1 is ready

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace szamlalas
 {
     internal class Program
     {
+        static int[] meres = { 36, 48, 39, -1, 30, 43, -1, 76, 67, 82, 73, 75, 64, 73, 69, 63 };
+
         static void Main(string[] args)
         {
         }
